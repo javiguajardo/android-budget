8d22dde93952d60d9f3d534640db93bf94b899a7
guajardo <a282057@uach.mx>
2017-05-27 16:48:14 -0600

    Add account to API

--- app/src/main/java/guajardo/budget/AddAcountActivity.java
+++ app/src/main/java/guajardo/budget/AddAcountActivity.java
@@ -9,7 +9,21 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import static guajardo.budget.AccountActivity.*;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
+
 
 public class AddAcountActivity extends AppCompatActivity {
     Spinner spinner;
@@ -36,14 +50,54 @@ public class AddAcountActivity extends AppCompatActivity {
         name = String.valueOf(accountName.getText());
         amount = String.valueOf(accountAmount.getText());
         accountType = spinner.getSelectedItem().toString();
-        Intent intent = new Intent(getBaseContext(), AccountActivity.class);
+        Intent i = new Intent(getBaseContext(), AccountActivity.class);
 
         if (name.matches("") || amount.matches("")) {
             Toast.makeText(this, "Favor de llenar todos los campos", Toast.LENGTH_SHORT).show();
             return;
+        } else {
+            sendData(name, amount, accountType);
+            startActivity(i);
         }
 
-        //Account.addAccount(accounts, new Account(name, accountType, Float.parseFloat(amount)));
-        startActivity(intent);
+    }
+
+    public void sendData(String name, String amount, String accountType) {
+        String url = "https://polar-sierra-78542.herokuapp.com/accounts";
+        RequestQueue queue = Volley.newRequestQueue(this);
+        Map<String, String> jsonParams = new HashMap<String, String>();
+
+        jsonParams.put("name", name);
+        jsonParams.put("amount", amount);
+        jsonParams.put("acct_type", accountType);
+
+        JsonObjectRequest postRequest = new JsonObjectRequest(Request.Method.POST, url,
+
+                new JSONObject(jsonParams),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(getApplicationContext(), "Cuenta agregada exitosamente",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //   Handle Error
+                        Toast.makeText(getApplicationContext(),
+                                error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json; charset=utf-8");
+                headers.put("User-agent", System.getProperty("http.agent"));
+                return headers;
+            }
+        };
+        queue.add(postRequest);
+
     }
 }
