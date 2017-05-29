4186aa26c0e970ef90d2673fa4713d45dacedbd7
guajardo <a282057@uach.mx>
2017-05-29 15:08:05 -0600

    Add in app notifications

--- app/src/main/java/guajardo/budget/MainActivity.java
+++ app/src/main/java/guajardo/budget/MainActivity.java
@@ -1,11 +1,16 @@
 package guajardo.budget;
 
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -28,6 +33,8 @@ import guajardo.budget.adapters.BudgetAdapter;
 import guajardo.budget.adapters.CategoryAdapter;
 import guajardo.budget.models.Category;
 import guajardo.budget.services.RegistrationService;
+
+import static android.R.attr.category;
 
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
@@ -94,10 +101,14 @@ public class MainActivity extends AppCompatActivity {
                         }
 
 
-                        for(int i = 0; i < jsonCategoriesArray.length(); i++) {
+                        for (int i = 0; i < jsonCategoriesArray.length(); i++) {
                             try {
                                 Category category = new Category(jsonCategoriesArray.getJSONObject(i));
                                 budgetAdapter.add(category);
+
+                                if (Double.parseDouble(category.getTotalAmount()) < 0) {
+                                    notificacionesGastos(i, category.getName(), category.getTotalAmount());
+                                }
 
 
                             } catch (JSONException e) {
@@ -122,5 +133,36 @@ public class MainActivity extends AppCompatActivity {
 
         // Add the request to the RequestQueue.
         queue.add(jsonObjectRequest);
+    }
+
+    public void notificacionesGastos(int notificationId, String categoryName, String categoryAmount) {
+        NotificationCompat.Builder mBuilder = (android.support.v7.app.NotificationCompat.Builder)
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(android.R.drawable.ic_dialog_alert)
+                        .setAutoCancel(true)
+                        .setContentTitle("Gastos")
+                        .setContentText("Te has excedido en tu presupuesto para " + categoryName + " por " + categoryAmount);
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity.class);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(notificationId, mBuilder.build());
     }
 }
